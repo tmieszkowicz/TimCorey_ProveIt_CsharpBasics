94be3c1d2fed466035963b31b29a52b3d5078df0
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2024-06-19 00:18:00 +0200

    add datetime challenge

--- datetime_challenge/Program.cs
+++ datetime_challenge/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datetime_challenge
+{
+    internal class Program
+    { 
+
+        public static string CalculateAndPrintDateTimeDifference()
+        {
+            Console.WriteLine("Enter date [dd/MM/yyyy HH:mm]");
+            string inputStringDate = Console.ReadLine();
+
+            DateTime inputDate = DateTime.ParseExact(inputStringDate, "dd/MM/yyyy HH:mm", System.Globalization.CultureInfo.InvariantCulture);
+
+            TimeSpan timeSpan = DateTime.Now - inputDate;
+
+
+            if (inputDate.Ticks < 0) 
+            {
+                return $"That was {(int)timeSpan.TotalDays} days ago.\nAnd {(int)timeSpan.TotalMinutes} minutes ago.";
+            }
+            else 
+            {
+                return $"That is {-(int)timeSpan.TotalDays} days in the future.\nAnd {-(int)timeSpan.TotalMinutes} minutes in the future.";
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine(CalculateAndPrintDateTimeDifference());
+        }
+    }
+}
