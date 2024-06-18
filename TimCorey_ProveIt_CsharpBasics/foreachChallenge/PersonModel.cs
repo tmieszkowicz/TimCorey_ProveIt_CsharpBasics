e501a1adc0573a890cb59eb574b448ebd3d08ea9
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2024-06-18 17:23:54 +0200

    Add project files.

---
+++ TimCorey_ProveIt_CsharpBasics/foreachChallenge/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimCorey_ProveIt_CsharpBasics.foreachChallenge
+{
+    internal class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public PersonModel(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+    }
+}
---
+++ TimCorey_ProveIt_CsharpBasics/foreachChallenge/foreachChallenge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TimCorey_ProveIt_CsharpBasics.foreachChallenge
+{
+    internal class foreachChallenge
+    {
+        public static List<PersonModel> GetPeopleStrings()
+        {
+            List<PersonModel> strings = new List<PersonModel>();
+
+            strings.Add(new PersonModel("Bob", "Smith"));
+
+            return strings;
+        }
+
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = GetPeopleStrings();
+
+
+            foreach (PersonModel p in people) 
+            { 
+                Console.WriteLine(p.FirstName + ' ' +  p.LastName);
+            }
+        }
+    }
+}
